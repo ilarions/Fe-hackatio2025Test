031da2f5d699d4d985400f0f63d4339a0a9e872e
artem <artemk2504@gmail.com>
2025-02-12 18:01:45 +0200

    fix

--- src/app/components/quizDescription/index.tsx
+++ src/app/components/quizDescription/index.tsx
@@ -1,5 +1,5 @@
 "use client"
- import { useParams, useRouter } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import axios from "axios";
 
 
@@ -12,29 +12,28 @@ interface IQuiz {
   rating: number,
   time: number,
   img: string
-
 }
 
 export const QuizDecription: React.FC<IQuiz> = ({ title, description, rating, time, img }) => {
-      const router = useRouter()
-   const { id } = useParams();
-   const hanlderStart = async () => {
+  const router = useRouter()
+  const { id } = useParams();
+  const hanlderStart = async () => {
     console.log("suka")
-     try {
-       const res = await axios.post("http://localhost:9000/room-quest/createroom",
-         { id_quiz: id },
-       { withCredentials: true },
-       )
-       console.log(res.data.newRoom.id)
-       router.push(`/gameblock/lobby/${res.data.newRoom.id}?token=${res.data.token}`)
-     } catch (error) { }
-   }
+    try {
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/room-quest/createroom`,
+        { id_quiz: id },
+        { withCredentials: true },
+      )
+      console.log(res.data.newRoom.id)
+      router.push(`/gameblock/lobby/${res.data.newRoom.id}/${res.data.token}`)
+    } catch (error) { }
+  }
 
   return (
     <Paper sx={{ width: "1300px", margin: "140px auto", display: "flex", justifyContent: "space-between", alignItems: "end", padding: "20px" }} elevation={12}>
       <Stack sx={{ display: "flex", justifyContent: "center", alignItems: "start", flexDirection: "row", gap: "40px" }}>
         {/* <Image src={img} alt="Quiz image" width={400} height={400} /> */}
-        <img src={img} alt="image" className='w-[400px] h-[400px]'/>
+        <img src={img} alt="image" className='w-[400px] h-[400px]' />
         <Stack sx={{ gap: "20px" }}>
           <Typography variant='h3' fontWeight={700}>{title}</Typography>
           <Typography variant='h6' lineHeight={1.45}>{description}</Typography>
