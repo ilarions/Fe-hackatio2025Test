74200cfc8b7c985d2e22fb8381868bdb9264d1df
stasik23 <kolesnikkosta572@gmail.com>
2025-02-12 20:16:19 +0200

    check

--- src/app/components/quizCard/index.tsx
+++ src/app/components/quizCard/index.tsx
@@ -13,7 +13,6 @@ export interface IQuiz {
 }
 
 export const QuizCard: React.FC<IQuiz> = ({ img, description, title, rating, id }) => {
-<<<<<<< HEAD
     const [loading, setLoading] = useState(true);
     useEffect(() => { setTimeout(() => setLoading(false), 2000) }, []);
     if (loading) { return <QuizCardSkeleton /> }
@@ -36,28 +35,5 @@ export const QuizCard: React.FC<IQuiz> = ({ img, description, title, rating, id 
             </CardContent>
         </Card>
     )
-=======
-  const [loading, setLoading] = useState(true);
-  useEffect(() => { setTimeout(() => setLoading(false), 2000) }, []);
-  if (loading) { return <QuizCardSkeleton /> }
 
-  return (
-    <Card sx={{ width: "300px", '&:hover': { boxShadow: "0 5px 1px 5px rgba(0,0,0,0.2)", transform: "translateY(-5px)", transition: ".3s ease-in-out" } }}>
-      <CardMedia image={img} sx={{ width: "full", height: "200px" }} />
-      <CardContent sx={{ gap: "40px", padding: "10px" }}>
-        <Stack sx={{ gap: "10px", marginBottom: "20px" }}>
-          <Typography variant="h5" sx={{ color: "#080614", fontWeight: "600" }}>{title}</Typography>
-          <Typography sx={{ color: "#A69DE2", fontWeight: "100", fontSize: "12px" }}>{description}</Typography>
-        </Stack>
-        <CardActions sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0px" }}>
-          <Stack sx={{ color: "#4635B1", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "row", gap: "3px" }}>
-            <StarOutlinedIcon />
-            <Typography sx={{ fontSize: "15px", fontWeight: "600" }}>{rating}</Typography>
-          </Stack>
-          <Link href={`/gameblock/${id}`}><Button variant="contained" sx={{ bgcolor: "#362989" }}>More about quiz</Button></Link>
-        </CardActions>
-      </CardContent>
-    </Card>
-  )
->>>>>>> 726c05c1b338c046001ed8bd183ec9ecfdd872cd
 }
