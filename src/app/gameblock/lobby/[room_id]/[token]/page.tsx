a82ff914a2742c5f128f81133232fbc35504f9bd
Max Ostapets <maksostapets2007@gmail.com>
2025-02-12 21:37:16 +0200

    quiz styles fixed

--- src/app/gameblock/lobby/[room_id]/[token]/page.tsx
+++ src/app/gameblock/lobby/[room_id]/[token]/page.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { Box, Button, hexToRgb } from '@mui/material'
+import { Box, Button, Stack, Typography, Paper } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import socketService from '../../../../../socket'
 import { useParams } from 'next/navigation';
+
 export default function Page() {
   const [quest, setQuest] = useState<any>(null)
   const handleQuestion = (response: any) => {
@@ -13,13 +14,15 @@ export default function Page() {
   const next_question = (res: any) => {
     console.log(res)
     setQuest(res.quest)
-
   }
+
   const end_quiz = (res: any) => {
     console.log("suka")
     console.log(res)
   }
+
   const { room_id, token } = useParams();
+
   useEffect(() => {
     socketService.connect();
     console.log(token)
@@ -32,17 +35,13 @@ export default function Page() {
     socketService.onMessage("start_question", handleQuestion);
     socketService.onMessage("next_question", next_question)
     socketService.onMessage("end_quiz", end_quiz)
-
-
-
     socketService.onMessage("end_quiz", (data) => {
       console.log("Received game update:", data);
     });
 
-    return () => {
-      socketService.disconnect();
-    };
+    return () => { socketService.disconnect() };
   }, [])
+
   const sendNextQuestion = (id: any) => {
     console.log(id)
     console.log(token)
@@ -55,24 +54,20 @@ export default function Page() {
   };
 
   return (
-    <Box className='w-full h-screen flex flex-col p-10 items-center justify-between bg-blue-400'>
-      <Box
-        sx={{ borderRadius: '5px' }}
-        className='bg-white w-2/3 flex flex-col items-start gap-2 justify-start p-10'>
+    <Stack sx={{ width: "full", bgcolor: "#C9C4EE" }}>
+      <Paper sx={{ borderRadius: '5px', width: "1200px", margin: "120px auto", padding: "20px"}}>
         {quest ?
-          <div>
-            <img src={quest.img} alt="" style={{ width: "350px", height: "400px" }} />
-            <h1 className='text-6xl text-black font-bold flex items-center justify-center mt-[50px]'>{quest?.title}</h1>
-            {quest.answers?.map((elem: any) => (
-              <Button onClick={() => sendNextQuestion(elem.id)} variant="contained" color="success" key={elem.id}>
-                {elem.text}
-              </Button>
+          <Stack sx={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "40px" }}>
+            <img src={quest.img} alt="" style={{ width: "250px", height: "300px" }} />
+            <Typography variant='h2'>{quest?.title}</Typography>
+            <Stack sx={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", width: "fit-content", gap: "20px" }}>
+              {quest.answers?.map((elem: any) => <Button onClick={() => sendNextQuestion(elem.id)} variant="contained" sx={{ bgcolor: "#362989", width: "300px" }} key={elem.id}>{elem.text}</Button>)}
+            </Stack>
+          </Stack>
 
-            ))}
-          </div>
-
-          : <h1>oops somethinggs wrong</h1>}
-      </Box>
-    </Box>
+          : <h1>oops somethinggs wrong</h1>
+        }
+      </Paper>
+    </Stack>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <Stack sx={{ margin: "110px auto", display: "grid", gridTemplateColumns: "repeat(3, 1fr)", width: "fit-content", gap: "50px" }}>
-      {quizzes.map((el: IQuiz) => <QuizCard img={el.img} description={el.description} title={el.title} rating={el.rating} key={el.id} />)}
+      {quizzes.map((el: IQuiz) => <QuizCard img={el.img} description={el.description} title={el.title} rating={el.rating} key={el.id} id={el.id}/>)}
     </Stack>
   )
 }
