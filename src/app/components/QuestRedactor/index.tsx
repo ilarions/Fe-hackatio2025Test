a01de439caa0e1d12c26787dc5508469f21db5d9
artem <artemk2504@gmail.com>
2025-02-12 16:50:18 +0200

    yep

--- src/app/components/QuestRedactor/index.tsx
+++ src/app/components/QuestRedactor/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Quest } from '@/type/quiz';
-import { CardHeader, Card, Box, Typography, Button, TextField } from '@mui/material';
+import { CardHeader, Card, Box, Typography, Button, TextField,Select,MenuItem } from '@mui/material';
 import { useForm } from 'react-hook-form';
 export default function QuestRedactor({ quest }: { quest: Quest[] | null }) {
+  const [stateCreate,setStateCreate]=useState<boolean>(false)
   console.log(quest)
   const {
     register,
@@ -10,21 +11,30 @@ export default function QuestRedactor({ quest }: { quest: Quest[] | null }) {
     watch,
     formState: { errors },
   } = useForm<any>()
-  const onSubmit = (data) => { }
+  const handlerChangerStateCreate=()=>setStateCreate(stateCreate)
+  const onSubmit = (data) => {console.log(data) }
   return (
     <Box>
-      <Button variant='contained' sx={{ bgcolor: "#362989", color: "white", padding: "10px 110px", marginTop: "10px", fontSize: "14px", fontWeight: "800" }}>add Item</Button>
-      <Card sx={{ display: "flex", flexDirection: "column", marginTop: "20px", alignItems: "center", width: "750px", background: "rgba(0, 0, 0, 0.1)", }}>
+      <Button onClick={handlerChangerStateCreate} variant='contained' sx={{ bgcolor: "#362989", color: "white", padding: "10px 110px", marginTop: "10px", fontSize: "14px", fontWeight: "800" }}>add Item</Button>
+      {stateCreate?<Card sx={{ display: "flex", flexDirection: "column",textAlign:"center", marginTop: "20px", alignItems: "center", width: "750px",height:"auto", background: "rgba(0, 0, 0, 0.1)", }}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Typography variant='h4' fontWeight={700}>
+      <TextField {...register("title")} label="Title" variant="outlined" required />
+
           </Typography>
-          <Box sx={{ display: "flex", flexWrap: "wrap", alignItems: "center", gap: "10px", marginTop: "20px", marginBottom: "20px", }}>
+          <Box sx={{ display: "flex", flexWrap: "wrap", alignItems: "start",gap:"10px",marginTop:"20px", marginBottom:"20px", width:"750px" ,height:"auto" }}>
             <Box sx={{ borderRadius: "8px", width: "370px", height: "50px", display: "flex", justifyContent: "center", alignItems: "center", textAlign: "center" }}>
-              <TextField {...register(`description`)} label="Description" multiline rows={3} variant="outlined" required />
+                   <TextField {...register("answer1")} label="Title" variant="outlined" required />
+                 <Select {...register("correct1")} defaultValue="20">
+        <MenuItem value='true'>true</MenuItem>
+        <MenuItem value="false`">false</MenuItem>
+      </Select>
             </Box>
           </Box>
+      <Button type='submit' variant='contained' sx={{ bgcolor: "#362989", color: "white", padding: "10px 110px", marginTop: "10px", fontSize: "14px", fontWeight: "800" }}>add Item</Button> 
+
         </form>
-      </Card>
+      </Card>:null}
       {quest?.map((elem) => (
         <Card key={elem.id} sx={{ display: "flex", flexDirection: "column", marginTop: "20px", alignItems: "center", width: "750px", background: "rgba(0, 0, 0, 0.1)", }}>
           <Typography variant='h4' fontWeight={700}>
